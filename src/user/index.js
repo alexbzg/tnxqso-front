31804ee768567efe41a9b3a1056ff61497c38874
root <alexbzg@gmail.com>
2017-11-21 18:12:24 +0300

    save user settings to db

--- src/user/index.js
+++ src/user/index.js
@@ -62,6 +62,22 @@ export default {
   saveSettings ( settings ) {
     _user.settings = JSON.parse( JSON.stringify( settings ) )
     toStorage()
+    var u = this
+    request.post( 'userSettings', _user )
+      .catch(function (error) {
+        var msg = ''
+        console.log(error)
+        if (error.status === 400) {
+          if ( error.message === 'Login expired' || error.message === 'Not logged in' ) {
+            u.logout()
+            return
+          }
+          msg = error.message
+        } else {
+          msg = 'Server error. Please try again later.'
+        }
+        alert(msg)
+      })
   }
 
 }

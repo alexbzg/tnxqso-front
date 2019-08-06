3d153c8182679f2de73fef91cc44f55d6809c7d8
root <alexbzg@gmail.com>
2019-08-06 16:34:07 +0300

    -

--- src/components/StationStats.vue
+++ src/components/StationStats.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="log">
-
+        <div id="refresh_time">Auto refresh<br/><b>1 min</b></div>
+        <div id="no_net" v-if="stationActive && !statusOnline" class="warning">
+          <div id="warning_border">
+            <span>There is no Internet connection to the station.</span> The data will be updated after the connection is restored.
+          </div>
+        </div>
         <div id="call_search">
             Callsign: 
             <input type="text" id="input_call" v-model="searchValue"> 
@@ -16,48 +21,122 @@
           </tr>
           <tr>
             <td>
-              <log-table :data="searchResults"></log-table>
+              <log-table :data="searchResults" :log-settings="stationSettings.log" 
+              v-if="stationSettings"></log-table>
             </td>
           </tr>
         </table>
 
-        <log-table :data="dataSlice"></log-table>
+        <log-table :data="dataSlice" :log-settings="stationSettings.log" v-if="stationSettings"></log-table>
 
     </div>
 </template>
 
 <script>
+import * as moment from 'moment'
+
 import tabMixin from '../station-tab-mixin'
+import LogTable from './LogTable'
 import storage from '../storage'
 
+const logSearchValueStorageKey = 'logSearchValue'
+const current = moment()
 
 export default {
   mixins: [tabMixin],
-  name: 'StationLog',
-  components: { LogTable },
+  name: 'StationStats',
+  props: ['stationSettings'],
   data () {
-    return {
-      tabId: 'log',
-      searchValue: storage.load( logSearchValueStorageKey, 'local' ),
-      searchResults: null
+    const fields = []
+    for (const field in this.stationSettings.log.columns) {
+      if (field !== 'loc' && this.stationSettings.log.columns[field])
+        fields.push(field)
     }
-  },
-  methods: {
-    search () {
-      this.searchValue = this.searchValue.toUpperCase()
-      storage.save( logSearchValueStorageKey, this.searchValue, 'local' )
-      this.searchResults = this.data.filter( spot => spot === this.searchValue )
-      if ( this.searchResults.length === 0 ) {
-        window.alert( 'Nothing was found.' )
-      }
-    },
-    clearSearch () {
-      this.searchResults = null
+    const userField = this.stationSettings.log.userColumns[0]
+    const userFieldName = this.stationSettings.userFields[0] || 'User field'
+    if (userField)
+      fields.push(userFieldName)
+    return {
+      tabId: 'stats',
+      fields: fields,
+      filter: {
+        band: null,
+        mode: null,
+        field: 'RDA',
+        fieldValue: null
+      },
+      userField: userField,
+      userFieldName: userFieldName
     }
   },
   computed: {
-    dataSlice () {
-      return this.data ? this.data.slice( 0, 50 ) : []
+    qsoCount () {
+      return this.data.length
+    },
+    csCount () {
+      return this.uniqueValues(x => x.cs).size
+    },
+    fieldValuesCount () {
+      const count = {}
+      for (const field of this.fields) {
+        if (field !== this.userFieldName) {
+          count[field] = this.uniqueVFieldalues(field).size
+        }
+      }
+    },
+    userFieldCount () {
+      return this.uniqueValues(x => x.userFields[0]).size
+    },
+    locatorsCount () {
+      return this.uniqueValues(x => x.loc).size
+    },
+    daysCount () {
+      return this.uniqueValues(x => x.date).size
+    },
+    filterFieldValues () {
+      const values = this.filter.field === this.userFieldName ?
+        this.uniqueValues(x => x.userFields[0]) :
+        this.uniqueFieldValues(field)
+      return Array.from(values).sort((a, b) => {
+          if (a > b) {
+            return 1
+          }
+          if (a === b) {
+            return 0
+          }
+          if (a < b) {
+            return -1
+          }
+        })
+    },
+    filteredQsoCount () {
+      return this.data.reduce((accumulator, qso) => {
+        if (!this.filter.band || qso.band === this.filter.band) {
+          if (!this.filter.mode || FULL_MODES[this.filter.mode].indexOf(qso.mode) !== -1) {
+            if (!this.filter.fieldValue || 
+              ((this.filter.field === this.userFieldName && this.filter.fieldValue === qso.userFields[0]) ||
+               (this.filter.field !== this.userFieldName && 
+                this.filter.fieldValue === qso[this.filter.field.toLowerCase()]))) {
+              accumulator++
+            }
+          }
+        }
+        return accumulator
+      })
+    }
+  },
+  methods: {
+    uniqueFieldValues (field) {
+      return this.uniqueValues(x => x[field.toLowerCase()], ', ')
+    },
+    uniqueValues (lambda, delimiter) {
+      if (delimiter) {
+        return new Set(this.data.reduce((accumulator, value) => {
+          value.split(delimiter).map(x => acummulator.push(x))
+          return accumulator
+        }))
+      }
+      return new Set(this.data.map(lambda))
     }
   }
 }
---
+++ src/ham-radio/index.js
@@ -0,0 +1,63 @@
+import rdaShort from '../rdaShort.json'
+
+export const BANDS = ['1.8', '3.5', '7', '10', '14', '18', '21', '24', '28']
+
+export function orderedBands() {
+  return BANDS.slice().sort((a, b) => {
+        return Number(a) - Number(b)
+      })
+}
+
+export const MODES = ['CW', 'SSB', 'DIGI']
+export const MODES_FULL = {
+  'CW': ['CW'],
+  'SSB': ['USB', 'SSB', 'LSB', 'AM', 'FM'],
+  'FT8': ['FT8']
+}
+export const ROLES = {hunter: 'Hunter', activator: 'Activator'}
+
+
+const rdaValues = []
+for (const group of rdaShort) {
+  for (let c = 1; c <= group.last; c++) {
+    if ('skip' in group && group.skip.includes(c)) {
+      continue
+    }
+    rdaValues.push(group.group + '-' + (c < 10 ? '0' + c : c))
+  }
+}
+export {rdaValues}
+
+const reRDA = /([a-z][a-z])[-_ ]?(\d\d)/gi
+
+export function parseRDA (val) {
+  let rdaMatch = null
+  let r = null
+  while ((rdaMatch = reRDA.exec(val)) !== null) {
+    let rda = (rdaMatch[1] + '-' + rdaMatch[2]).toUpperCase()
+    if (rdaValues.includes(rda)) {
+      r = rda
+      break
+    }
+  }
+  reRDA.lastIndex = 0
+  return r
+}
+
+
+const reSTRIP_CALLSIGN = /\d*[A-Z]+\d+[A-Z]+/
+
+export function stripCallsign(callsign) {
+    let match = null
+    if (match = reSTRIP_CALLSIGN.exec(callsign))
+        return match[0]
+    else
+        return null
+}
+
+const reCALLSIGN_FULL = /^(:?[A-Z\d]+\/)?\d?[A-Z]+\d+[A-Z]+(:?\/[A-Z\d]+)*$/
+
+export function validCallsignFull (callsign) {
+  return reCALLSIGN_FULL.test(callsign)
+}
+

53e1110c64015c5d4f03fbf695b4a7b50c96e783
root <alexbzg@gmail.com>
2019-09-11 13:35:32 +0300

    deployment script fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 chown web_ftp:www-data src -R
 chmod o+w src -R
-chmod g+w public -R
 rm /var/www/tnxqso/static/js/*.js
 rm /var/www/tnxqso/static/js/*.js.map
-cp /var/www/tnxqso-test/dist/js/*.js /var/www/tnxqso/static/js/
-cp /var/www/tnxqso-test/dist/js/*.js.map /var/www/tnxqso/static/js/
+cp /var/www/tnxqso-test/dist/static/js/*.js /var/www/tnxqso/static/js/
+cp /var/www/tnxqso-test/dist/static/js/*.js.map /var/www/tnxqso/static/js/
 rm /var/www/tnxqso/static/css/*
 cp /var/www/tnxqso-test/dist/static/css/* /var/www/tnxqso/static/css/
 cp /var/www/tnxqso-test/dist/static/images/* /var/www/tnxqso/static/images

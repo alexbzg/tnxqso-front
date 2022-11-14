a216f4cf3d5546a806074973ca7e63bc8fd018ee
root <root@194-67-78-213.cloudvps.regruhosting.ru>
2022-11-14 14:32:25 +0300

    feat: active stations filter by station type (/P-/M-other)

--- src/views/ActiveStations.vue
+++ src/views/ActiveStations.vue
@@ -1,35 +1,50 @@
 <template>
     <div>
-        <div id="view" @click="toggleCompactView">
-           <img v-if="compactView"  id="view_full" src="/static/images/icon_view_full.png" />
-           <img v-else id="view_compact" src="/static/images/icon_view_compact.png" />
+
+        <div id="view_buttons">
+          <div 
+            v-for="(value, index) in stationsTypesFilter"
+            @click="toggleStationsTypesFilter(index)"
+            :class="{ selected_view: value }"
+            :key="index">
+            <img 
+                :src="$options.STATIONS_TYPES_FILTER[index].image" 
+                :title="$options.STATIONS_TYPES_FILTER[index].title" />
+          </div>
+
+          <!--div id="view" @click="toggleCompactView">
+            <img v-if="compactView"  id="view_full" src="/static/images/icon_view_full.png" />
+            <img v-else id="view_compact" src="/static/images/icon_view_compact.png" />
+          </div-->
         </div>
 
         <div id="active_stations" class="stations_list">
             <template v-for="(stations, idx0) in activeStationsStatus">
-                <active-station-entry v-for="(item, idx) in stations"
+                <active-station-entry v-for="(item, idx) in filterStations(stations)"
                     :station-settings="item" :site-admin="siteAdmin" type="active"
-                    :compactView="compactView"
+                    :compactView="true"
                     @publish-change="publishChange(item)" :key="idx0 + '_' + idx">
                 </active-station-entry>
-                <br 
+                <br
                     v-if="idx0 === 0"
                     :key="idx0"/>
             </template>
         </div>
         <div id="future_stations" class="stations_list">
             <div class="stations_block">Coming soon</div>
-            <active-station-entry v-for="(station, index) in $store.state.activeStations.stations.future"
+            <active-station-entry 
+                v-for="(station, index) in filterStations($store.state.activeStations.stations.future)"
                 :station-settings="station" :site-admin="siteAdmin" type="inactive"
-                :compactView="compactView"
+                :compactView="true"
                 @publish-change="publishChange(station)" :key="index">
             </active-station-entry>
         </div>
         <div id="archive_stations" class="stations_list">
             <div class="stations_block">Archive</div>
-            <active-station-entry v-for="(station, index) in $store.state.activeStations.stations.archive"
+            <active-station-entry 
+                v-for="(station, index) in filterStations($store.state.activeStations.stations.archive)"
                 :station-settings="station" :site-admin="siteAdmin" type="archive"
-                :compactView="compactView"
+                :compactView="true"
                 @publish-change="publishChange(station)" :key="index">
             </active-station-entry>
        </div>
@@ -46,13 +61,37 @@ import {MUTATE_ACTIVE_STATIONS_READ} from '../store-active-stations'
 import ActiveStationEntry from '../components/ActiveStationEntry'
 
 const STORAGE_KEY_COMPACT_VIEW = 'activeStationsCompactView'
+const STORAGE_KEY_STATIONS_TYPES_FILTER = 'activeStationsStationTypeFilter'
+
+const STATIONS_TYPES_FILTER = [
+    {
+        filter: /\/M$/,
+        image: "/static/images/icon_view_mobile.png",
+        title: "Show mobile stations"
+    },
+    {
+        filter: /\/P$/,
+        image: "/static/images/icon_view_portable.png",
+        title: "Show portable stations"
+    },
+    {
+        filter: /(?<!\/[MP])$/,
+        image: "/static/images/icon_view_stationary.png",
+        title: "Show stationary stations"
+    }
+]
 
 export default {
   name: 'activeStations',
   components: {ActiveStationEntry},
+  STATIONS_TYPES_FILTER: STATIONS_TYPES_FILTER,
   data () {
+    const stationsTypesFilter = storage.load(STORAGE_KEY_STATIONS_TYPES_FILTER, 'local') || 
+        STATIONS_TYPES_FILTER.map( () => true )
+
     return {
-      compactView: storage.load(STORAGE_KEY_COMPACT_VIEW, 'local')
+      compactView: storage.load(STORAGE_KEY_COMPACT_VIEW, 'local'),
+      stationsTypesFilter: stationsTypesFilter
     }
   },
   activated () {
@@ -63,6 +102,9 @@ export default {
   },
   computed: {
     ...mapGetters(['siteAdmin', 'activeStationsRead', 'activeStationsStatus']),
+    filteredStations () {
+      return []
+    }
   },
   methods: {
     ...mapMutations([MUTATE_ACTIVE_STATIONS_READ]),
@@ -79,6 +121,14 @@ export default {
         path: 'publish',
         data: {station: station.station.callsign, publish: station.publish}
       })
+    },
+    toggleStationsTypesFilter (index) {
+      this.$set(this.stationsTypesFilter, index, !this.stationsTypesFilter[index])
+      storage.save(STORAGE_KEY_STATIONS_TYPES_FILTER, this.stationsTypesFilter, 'local')
+    },
+    filterStations (stations) {
+      return stations.filter( item => this.stationsTypesFilter.some( (value, index) => 
+        value && STATIONS_TYPES_FILTER[index].filter.test(item.station.callsign) ) )
     }
   },
   watch: {
@@ -88,7 +138,7 @@ export default {
       }
     }
   }
- 
+
 }
 </script>
 

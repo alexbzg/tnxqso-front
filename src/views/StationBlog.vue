a030a7ed7739610a1ea760b64744e013936ff73a
root <root@194-67-78-213.cloudvps.regruhosting.ru>
2023-07-13 11:49:46 +0300

    StationBlog rework

--- src/views/StationBlog.vue
+++ src/views/StationBlog.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="gallery">
+    <div id="blog">
         <div id="add_image" v-if="isAdmin" @click="showUpload = !showUpload">
-            <img src="/static/images/icon_add_image.png" width='50' />
+            <img src="/static/images/icon_add_image.png" title="New message" />
         </div>
 
         <div class="media_upload_form" v-if="isAdmin && showUpload">
@@ -16,43 +16,44 @@
                 :disabled="posting || (!upload.file && !upload.caption)" @click="uploadPost"/>
         </div>
 
-        <div class="media" v-for="(item, idx) in serviceData" :key="idx">
+        <div class="media" v-for="(item, idx) in serviceData" :key="idx"
+            @click="openModal(item)">
             <img class="delete" src="/static/images/delete.png" width='30' v-if="isAdmin"
-                @click="deleteItem(item.id)"/>
+                @click.stop="deleteItem(item.id)"/>
 
             <template v-if="item.file">
                 <img class="icon_video" src="/static/images/icon_video.png" width='80'
-                    v-if="item.type === 'video'"  @click="openLightbox(idx)"/>
+                    v-if="item.type === 'video'"/>
 
-                <img :src="stationPath + item.thumb" @click="openLightbox(idx)"/>
+                <img :src="stationPath + item.thumb"/>
 
-                <div class="date" v-if="item.datetime">{{item.datetime}}</div>
             </template>
 
+            <div class="date" v-if="item.datetime">{{item.datetime}}</div>
             <div class="caption">{{item.caption}}</div>
         </div>
 
 
-        <vue-pure-lightbox :images="lbImages" :open-at-index="lbIndex" ref="lb">
-            <template v-slot:loader>Loading…</template>
-            <template v-slot:content="{url: {link, alt, type, width}}">
-                <div class="imgbox">
-                    <span class="helper"></span>
-                    <img :src="link" :alt="alt" v-if="type === 'image'"/>
-                    <video v-else controls :src="link" :width="width"/>
-                </div>
+        <Modal 
+            v-if="activePost != null"
+            @close="closeModal()"
+        >
+            <template v-slot:body>
+                <template v-if="activePost.type">
+                    <img :src="stationPath + activePost.file" v-if="activePost.type === 'image'"/>
+                    <video v-else controls :src="stationPath + activePost.file" :width="activePost.width"/>
+                </template>
+                <div class="caption">{{activePost.caption}}</div>
             </template>
-        </vue-pure-lightbox>
+        </Modal>
     </div>
 </template>
 
 <script>
 import {mapActions, mapState, mapGetters} from 'vuex'
 
-import VuePureLightbox from 'vue-pure-lightbox'
-import 'vue-pure-lightbox/dist/VuePureLightbox.css'
-
 import ServiceDisplay from './ServiceDisplay'
+import Modal from '../components/Modal'
 
 import {ACTION_POST} from '../store-user'
 import {ACTION_UPDATE_SERVICE} from '../store-services'
@@ -61,7 +62,7 @@ import {urlCallsign} from '../utils'
 
 export default {
   extends: ServiceDisplay,
-  components: {VuePureLightbox},
+  components: {Modal},
   name: 'StationGallery',
   data () {
     return {
@@ -71,7 +72,8 @@ export default {
         file: null,
         caption: null
       },
-      posting: false
+      posting: false,
+      activePost: null
     }
   },
   computed: {
@@ -82,27 +84,20 @@ export default {
     stationPath () {
       return '/static/stations/' + urlCallsign(this.stationCallsign) + '/'
     },
-    _lbImages () {
-      return this.serviceData.map(x => {
-        return this.stationPath + x.file
-      })
-    },
-    lbImages () {
-      return this.serviceData.map(x => {
-        return {
-          link: this.stationPath + x.file,
-          alt: x.caption,
-          type: x.type,
-          width: x.type === 'video' ? x.width : null
-        }
-      })
-    },
     isAdmin () {
       return isAdmin()
     }
   },
   methods: {
     ...mapActions([ACTION_POST, ACTION_UPDATE_SERVICE]),
+    openModal (item) {
+      this.activePost = item
+      document.body.classList.add('modal-open')
+    },
+    closeModal () {
+      this.activePost = null
+      document.body.classList.remove('modal-open')
+    },
     uploadFileChange () {
       this.upload.file = this.$refs.fileInput.files[0]
     },
@@ -121,6 +116,7 @@ export default {
         .then(() => {
           this.upload.file = null
           this.upload.caption = null
+          this.showUpload = false
         })
     },
     deleteItem (id) {
@@ -134,22 +130,4 @@ export default {
 </script>
 
 <style>
-.imgbox {
-   width:90vw;
-   height:100vh;
-   white-space: nowrap;
-   text-align:center;
-}
-.imgbox .helper {
-   display: inline-block;
-   height: 100%;
-   vertical-align: middle;
-}
-.imgbox img, .imgbox video {
-   max-width:100%;
-   max-height:100%;
-   width:auto !important;
-   height:auto !important;
-   vertical-align: middle;
-}
 </style>

559efcf261e4bd521774f217c34e090aa3b6b529
root <root@194-67-78-213.cloudvps.regruhosting.ru>
2023-09-26 15:23:00 +0300

    chat: convert this site urls to links

--- src/chat-utils/index.js
+++ src/chat-utils/index.js
@@ -3,20 +3,23 @@ import sanitizeHTML from 'sanitize-html'
 import {SMILIES_IMG_PATH} from '../components/Smilies'
 
 const RE_MSG_TO = /(:?\u21d2\s?\w+(:?\/\w+)*\s?)+(:?\s|$)/
+const RE_URL = new RegExp(`${window.location.protocol}//${window.location.host}/\\S*`, "g")
 
 const MSG_SANITIZE_HTML_SETTINGS = {
   allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
     'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
     'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img'],
   allowedAttributes: {
-    img: ['src']
+    img: ['src'],
+    a: ['href', 'target']
   }
 }
+
 
 export function parseMsgText (text, smilies=true) {
   text = sanitizeHTML(text, MSG_SANITIZE_HTML_SETTINGS)
   if (smilies)
-    text = text.replace(/:(\d\d):/g, '<image src="' + SMILIES_IMG_PATH + '$1.gif"/>')
+    text = text.replace(/:(\d\d):/g, '<img src="' + SMILIES_IMG_PATH + '$1.gif"/>')
 
   let match = RE_MSG_TO.exec(text)
   let to = []
@@ -27,6 +30,7 @@ export function parseMsgText (text, smilies=true) {
     to.shift()
     to = to.map(item => item.trim())
   }
+  text = text.replace(RE_URL, '<a href="$&">$&</a>')
   return { text, to }
 }
 
